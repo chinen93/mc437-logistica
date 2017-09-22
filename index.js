40beba6b29c99352d6af49db0b9c9ba68b07b1a9
Pedro Chinen <chinen93@gmail.com>
2017-09-22 14:41:46 -0300

    Tutorial about pug

---
+++ index.js
@@ -0,0 +1,17 @@
+var pug = require('pug'),
+  fs = require('fs'); 
+var data = {
+  title: "Practical Node.js",
+  author: {
+    twitter: "@azat_co",
+    name: "Azat"
+  },
+  tags: ['express', 'node', 'javascript']
+}
+data.body = process.argv[2];
+
+fs.readFile('template.pug', 'utf-8', function(error, source){
+  var template = pug.compile(source);
+  var html = template(data)
+  console.log(html)
+});
