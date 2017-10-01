c904eebd486f75e738d3074af1df3d2995aea7c6
Lucas Brito <lucasbfmatos@hotmail.com>
2017-10-01 07:30:19 -0300

    fixed aws credentials

--- models/MysqlDatabase.js
+++ models/MysqlDatabase.js
@@ -2,7 +2,13 @@ const mysql = require('mysql');
 const credentials = require('./mariaDBCredentials.json');
 
 exports.execSQLQuery = function (sqlQry, resCallback) {
-  const connection = mysql.createConnection(credentials);
+  const connection = mysql.createConnection({
+    host: process.env.RDS_HOSTNAME || credentials.host,
+    port: process.env.RDS_PORT || credentials.port,
+    user: process.env.RDS_USERNAME || credentials.user,
+    password: process.env.RDS_PASSWORD || credentials.password,
+    database: process.env.RDS_DB_NAME || credentials.database
+  });
 
   connection.query(sqlQry, (error, results) => {
     resCallback(error, results);
